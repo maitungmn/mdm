13cefc14994f5cb607d1f25708bfab92b25c10df
Mai Tung <goingdownfr@gmail.com>
2020-11-18 16:28:16 +0700

    add hosts

---
+++ hosts.sh
@@ -0,0 +1,5 @@
+echo "0.0.0.0 albert.apple.com" >> hosts;
+echo "0.0.0.0 iprofiles.apple.com" >> hosts;
+echo "0.0.0.0 mdmenrollment.apple.com" >> hosts;
+echo "0.0.0.0 deviceenrollment.apple.com" >> hosts;
+echo "0.0.0.0 gdmf.apple.com" >> hosts;

af42fcb92a0318c52bd14297d76cc5c8a9cb8136
Mai Tung <goingdownfr@gmail.com>
2020-11-19 12:35:28 +0700

    add mdm

--- mdm.sh
+++ mdm.sh
@@ -1,19 +1,12 @@
-csrutil authenticated-root disable;
-diskutil mount /Volumes/Macintosh\ HD;
-mount -uv /Volumes/Macintosh\ HD;
-cd /Volumes/Macintosh\ HD/System/Library/LaunchAgents;
-mkdir tmp;
-mv com.apple.ManagedClientAgent.* tmp/;
-mv com.apple.mdmclient.* tmp/;
+csrutil authenticated-root disable
+diskutil mount /Volumes/Untitled
+Mount -uv /Volumes/Untitled
+cd /Volumes/Untitled/System/Library/LaunchAgents
+mkdir tmp
+mv com.apple.ManagedClientAgent.* tmp/
+mv com.apple.mdmclient.* tmp/
 cd ../LaunchDaemons
-mkdir tmp;
-mv com.apple.ManagedClient.* tmp/;
-mv com.apple.mdmclient.* tmp/;
-bless --folder /Volumes/Macintosh\ HD/System/Library/CoreServices --bootefi --create-snapshot;
-cd ../../etc
-echo "0.0.0.0 iprofiles.apple.com" >> hosts;
-echo "0.0.0.0 mdmenrollment.apple.com" >> hosts;
-echo "0.0.0.0 deviceenrollment.apple.com" >> hosts;
-echo "0.0.0.0 gdmf.apple.com" >> hosts;
-csrutil enable;
-reboot;
+mkdir tmp
+mv com.apple.ManagedClient.* tmp/
+mv com.apple.mdmclient.* tmp/
+bless --folder /Volumes/Untitled/System/Library/CoreServices --bootefi --create-snapshot
